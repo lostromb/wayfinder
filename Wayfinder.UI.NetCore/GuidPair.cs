0a87fa189fc2a9f33fff93a398259143fb2d2e58
Logan Stromberg <lostromb@microsoft.com>
2021-09-10 12:06:51 -0700

    Adding new UI project, stubbing out QFont for now

---
+++ Wayfinder.UI.NetCore/GuidPair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayfinderUI
+{
+    public struct GuidPair
+    {
+        public Guid A;
+        public Guid B;
+
+        public GuidPair(Guid a, Guid b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != typeof(GuidPair))
+            {
+                return false;
+            }
+
+            GuidPair other = (GuidPair)obj;
+            return (A == other.A && B == other.B) ||
+                (B == other.A && A == other.B);
+        }
+
+        public override int GetHashCode()
+        {
+            return A.GetHashCode() ^ B.GetHashCode();
+        }
+    }
+}
---
+++ Wayfinder.UI.NetCore/ModalDialogPromptFilePath.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WayfinderUI
+{
+    /// <summary>
+    /// Interaction logic for ModalDialogPromptFilePath.xaml
+    /// </summary>
+    public partial class ModalDialogPromptFilePath : Window
+    {
+        public bool Submitted = false;
+        public string Path { get; private set; }
+
+        public ModalDialogPromptFilePath()
+        {
+            InitializeComponent();
+            Path = string.Empty;
+            PathTextBox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Submitted = true;
+            Path = PathTextBox.Text;
+            this.Close();
+        }
+    }
+}
